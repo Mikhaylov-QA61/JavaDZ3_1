52da841742d999ae96add48a3d89fce747bb5a13
Pers test <qa61test@gmail.com>
2023-04-18 12:55:00 +0400

    initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,13 @@
+public class BonusMilesService {
+
+    public int calculate(int costTiket) {
+
+        int miles;
+        if (costTiket > 20) {
+            miles = costTiket / 20;
+        } else {
+            miles = 0;
+        }
+        return miles;
+    }
+}
